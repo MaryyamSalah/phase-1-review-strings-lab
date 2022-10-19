0525668d703f3d467ea71cf3c178b7e4d0d0b290
MaryyamSalah <maryyamslah20@gmail.com>
2022-10-19 13:37:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Maryam Salah';
+const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = 'Welcome, ${currentUser.slice(0, 1)}!';
